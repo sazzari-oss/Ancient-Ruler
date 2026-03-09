50c8c58c8bcc4740e9000b94c6a67935a65da045
sazzari-oss <sazzari@choicecharterschool.org>
2026-03-09 17:28:50 -0400

    Create History Challenge component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HistoryChallenge = () => {
+    return (
+        <div>
+            <h1>History Challenge</h1>
+            <p>Test your knowledge of historical events!</p>
+            {/* Add your quiz or challenge components here */}
+        </div>
+    );
+};
+
+export default HistoryChallenge;
